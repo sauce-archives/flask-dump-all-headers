a2738e7369f2bc2ab39b94e22d3fe590ed45a59f
Gavin Mogan <gavin@saucelabs.com>
2018-05-29 20:06:10 -0700

    Dump environmental variables too

--- run.py
+++ run.py
@@ -1,16 +1,22 @@
 from werkzeug.contrib.fixers import ProxyFix
 from flask import Flask, request, jsonify
+from six import string_types, text_type, integer_types
+import os
+
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
 @app.route('/')
-def hello_world():
+def dump():
+    environ = {k: v for k, v in request.environ.items()
+               if isinstance(v, text_type) or isinstance(v, string_types) or isinstance(v, integer_types)}
     req_data = {}
     req_data['endpoint'] = request.endpoint
     req_data['method'] = request.method
     req_data['cookies'] = request.cookies
     req_data['data'] = request.data
+    req_data['environ'] = dict(environ)
     req_data['headers'] = dict(request.headers)
     req_data['headers'].pop('Cookie', None)
     req_data['args'] = request.args
